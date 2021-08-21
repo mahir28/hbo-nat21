99618ef18252f90f7f1a10fbcde870c5020df6e3
Mahir Shahriar <41387794+mahirbd@users.noreply.github.com>
2021-08-22 03:37:49 +0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 24, 2021 14:55:00").getTime();
+var countDownDate = new Date("Aug 22, 2021 14:55:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
